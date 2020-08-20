e3f0e9b85ade6a9f865a9d1846ef78690810fcff
Lukas <lukas.buehne@mailbox.org>
2020-08-20 14:24:26 +0200

    Backup mobil drive

---
+++ backup_mobil.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#
+#
+#Start des Log Eintrags
+/usr/bin/logger Backup Mobil - Start am `date`
+
+#Backup Start Log in Cloud
+echo ------------------------------------------------------------ >> /mnt/nextclouddata/clouddata/__groupfolders/12/Log/Backup.txt
+echo $(date)": Mobil Backup wird gestartet." >> /mnt/nextclouddata/clouddata/__groupfolders/12/Log/Backup.txt
+
+
+#Mounten
+/bin/mount -t auto /dev/$1 /mnt/backup
+
+#Backup
+/usr/bin/logger Backup Mobil - Nextcloud Daten
+/usr/bin/rsync -rtv --del --modify-window=2 /mnt/nextclouddata/clouddata /mnt/backup #Daten
+/usr/bin/rsync -rtv --del --modify-window=2 /var/www/html /mnt/backup/installver #Installationsverzeichnis
+
+#
+/bin/sync
+
+#Datenbank speichern
+mysqldump --single-transaction -h localhost -u nextcloud -plukas nextcloud > /mnt/backup/db_backup_`date +"%Y%m%d"`.sql
+
+#Unmounten
+/bin/umount /mnt/backup
+
+#Piepen
+beep -f130 -l100 -n -f262 -l100 -n -f330 -l100 -n -f392 -l100 -n -f523  -l100 -n -f660 -l100 -n -f784 -l300 -n -f660 -l300 -n -f146 -l100 -n -f262 -l100 -n -f311 -l1$
+
+#End Log
+/usr/bin/logger Backup Mobil - Ende am `date`
+
+#Backup End Log in Cloud
+echo $(date)": Mobil Backup wird beendet." >> /mnt/nextclouddata/clouddata/__groupfolders/12/Log/Backup.txt
+echo ------------------------------------------------------------ >> /mnt/nextclouddata/clouddata/__groupfolders/12/Log/Backup.txt
